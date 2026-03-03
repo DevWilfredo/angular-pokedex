76e1e296b777ca2a81ef0bd620fc2e9125771306
DevWilfredo <wilfredopintomata@gmail.com>
2026-03-03 16:56:48 +0100

    core #2: arquitectura base de carpetas definida

---
+++ src/app/features/pages/pokemon-details/pokemon-details/pokemon-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  imports: [],
+  templateUrl: './pokemon-details.html',
+  styleUrl: './pokemon-details.css',
+})
+export class PokemonDetails {}
